b264f34b399a1484f75e9a0e7bb563d845e956ec
nitinmakin <nitinmakin124.com>
2020-08-31 19:25:37 +0530

    Added-Find max in given 3 float value

--- src/main/java/com/bridgelabz/generics/service/MaximumValue.java
+++ src/main/java/com/bridgelabz/generics/service/MaximumValue.java
@@ -11,4 +11,14 @@ public class MaximumValue
         else
             return c;
     }
+
+    public float getMaxFloat(Float a, Float b, Float c)
+    {
+        if(a.compareTo(b) > 0 && b.compareTo(c) > 0)
+            return a;
+        else if (b.compareTo(c) > 0)
+            return b;
+        else
+            return c;
+    }
 }
--- src/test/java/com/bridgelabz/generics/test/MaximumValueTest.java
+++ src/test/java/com/bridgelabz/generics/test/MaximumValueTest.java
@@ -12,19 +12,45 @@ public class MaximumValueTest {
             maximumValue = new MaximumValue();
         }
 
+        /**
+         * for checking integer
+         */
+
         @Test
         public void whenGivenNo_InFirstPosition_thenReturnSameNumber()
         {
-            Assert.assertEquals(40,maximumValue.getMaxInt(40,20,10));
+                Assert.assertEquals(40,maximumValue.getMaxInt(40,20,10));
         }
         @Test
         public void whenGivenNo_InSecondPosition_thenReturnSameNumber()
         {
-            Assert.assertEquals(40,maximumValue.getMaxInt(20,40,10));
+                Assert.assertEquals(40,maximumValue.getMaxInt(20,40,10));
         }
         @Test
         public void whenGivenNo_InThirdPosition_thenReturnSameNumber()
         {
-            Assert.assertEquals(40,maximumValue.getMaxInt(10,20,40));
+                Assert.assertEquals(40,maximumValue.getMaxInt(10,20,40));
         }
+
+        /**
+         * checking for float number
+         */
+
+        @Test
+        public void whenGivenDoubleNo_InFirstPosition_thenReturnSameNumber()
+        {
+                Assert.assertEquals(12.5f,maximumValue.getMaxFloat(12.5f,4.5f,0.1f), 0.0f);
+        }
+        @Test
+        public void whenGivenDoubleNo_InSecondPosition_thenReturnSameNumber()
+        {
+                Assert.assertEquals(40.5f,maximumValue.getMaxFloat(20.2f,40.5f,10.2f),0.0f);
+        }
+        @Test
+        public void whenGivenDoubleNo_InThirdPosition_thenReturnSameNumber()
+        {
+                Assert.assertEquals(40.5f,maximumValue.getMaxFloat(20.2f,10.5f,40.5f),0.0f);
+        }
+
+
 }
